a99c8976ea59a73dd9eeec0a9d26b672d4f812d1
Aryan-Sayeed <aryan.changes@gmail.com>
2025-07-12 13:58:31 +0600

    Add files via upload

---
+++ C++/Practice Projects/Movie ticket Update and cost/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+class movieticket{
+//movie name, seat number, price.
+
+string name;
+string seatNo;
+int price;
+
+public:
+    void setName(string n){
+    name= n;
+    }
+     void setSeat(string s){
+    seatNo = s;
+    }
+    void setPrice(int p){
+    price = p;
+    }
+//Method printTicket() that shows all details.
+    void printTicket(){
+    cout<<"Movie Name: "<<name<<endl;
+    cout<<"Seat Name: "<<seatNo<<endl;
+    cout<<"Price: "<<price<<endl;
+    cout<<"\n"<<endl;
+    }
+   // Method upgradeSeat() — changes seat number and price
+   void upgradeSeat(string ns, int np ){
+   setSeat(ns);
+   setPrice(np);
+    cout<<"Movie Name: "<<name<<endl;
+    cout<<"New Seat Name: "<<seatNo<<endl;
+    cout<<" New Price: "<<price<<endl;
+    cout<<"\n"<<endl;
+
+   }
+};
+
+
+
+int main()
+{
+movieticket m1;
+m1.setName("Rio");
+m1.setSeat("A5 row 20");
+m1.setPrice(20);
+m1.printTicket();
+m1.upgradeSeat("C5 row 2",5);
+
+    return 0;
+}
